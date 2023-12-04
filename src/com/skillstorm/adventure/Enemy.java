fc8fc1923f5eb985479839c6fe875daa989bc729
sod2003 <sod2003@gmail.com>
2023-12-03 22:43:32 -0500

    Modified Enemy class to add a Type variable

--- src/com/skillstorm/adventure/Enemy.java
+++ src/com/skillstorm/adventure/Enemy.java
@@ -1,11 +1,21 @@
 package com.skillstorm.adventure;
 
 public class Enemy extends Actor {
+	private String type;
 	private String treasure;
 
-	public Enemy(int muscle, int mysticality, int moxie, String treasure) {
+	public Enemy(int muscle, int mysticality, int moxie, String type, String treasure) {
 		super(muscle, mysticality, moxie);
+		this.type = type;
 		this.treasure = treasure;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getTreasure() {
