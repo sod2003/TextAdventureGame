fe89a61f51709ca3d83be18dc42f4167fe59f33b
sod2003 <sod2003@gmail.com>
2023-12-04 11:37:54 -0500

    Modified the battle method to return a boolean, add print statements, and give defeated enemy treasure to player.

--- src/com/skillstorm/adventure/BattleHandler.java
+++ src/com/skillstorm/adventure/BattleHandler.java
@@ -21,19 +21,21 @@ public class BattleHandler {
 		this.enemiesDefeated = enemiesDefeated;
 	}
 	
-	public String battle(Player player, Enemy enemy, Scanner scanner) {
+	public boolean battle(Player player, Enemy enemy, Scanner scanner) {
 		battleCount++;
-		String result = "";
+		boolean result = false;
 		System.out.format("You square off with the %s.\nRolling attacks...\n",
 				enemy.getType());
 		double playerAttack = (Math.random() + 1) * player.getMuscle();
 		double enemyAttack = (Math.random() + 1) * enemy.getMuscle();
 		if ( playerAttack > enemyAttack) {
 			System.out.format("You defeated the %s!\n", enemy.getType());
-			result = "success";
+			result = true;
+			System.out.format("You gained a %s from the defeated %s.\n",
+					enemy.getTreasure(), enemy.getType());
+			player.addInventory(new String[] {enemy.getTreasure()});
 		} else {
 			System.out.format("The %s defeated you!\n", enemy.getType());
-			result = "defeat";
 		}
 		return result;
 	}
