76943ab1563d5e661ff31151be6c06def9f5d829
sod2003 <sod2003@gmail.com>
2023-12-05 11:17:32 -0500

    Adding ScenarioHandler class

---
+++ src/com/skillstorm/adventure/ScenarioHandler.java
@@ -0,0 +1,19 @@
+package com.skillstorm.adventure;
+
+public class ScenarioHandler {
+	Scenario[] scenes;
+
+	public ScenarioHandler(Scenario[] scenes) {
+		super();
+		this.scenes = scenes;
+	}
+
+	public Scenario[] getScenes() {
+		return scenes;
+	}
+
+	public void setScenes(Scenario[] scenes) {
+		this.scenes = scenes;
+	}
+	
+}
